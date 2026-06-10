00414060e063aa30d24ac0424568b02f75ebf85e
aditi2902 <140902024+aditi2902@users.noreply.github.com>
2026-06-10 10:46:50 +0530

    Add: non-overlapping-intervals - submission-3

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        
+        ## when 2 overlap remove the interval ending last
+        ## x1 x2 y1 y2  : x2 > y1 overlap
+        ##  x1 x2 y1 y2 : x2 <= y1
+        ## cant delete within list as for i 0->n the n keeps reducing => index out of bound
+        ## NEED TO SORT
+        ##KEEP TRACK OF PREV END
+        count = 0
+        n=len(intervals)
+        intervals.sort()
+        if n==0:
+            return 0
+        prev_end = intervals[0][1]
+        for x in range(1,n):
+            if intervals[x][0]<prev_end:
+                count+=1
+                if intervals[x][1]<prev_end:
+                    prev_end=intervals[x][1]
+            else:
+                prev_end=intervals[x][1]  
+            
+        return count
+
